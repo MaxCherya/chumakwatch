a616bb01d3aceb1c667080f55f78f5ef5a985970
MaxCherya <maxchergik@gmail.com>
2024-11-11 15:19:03 +0200

    Clean view for frontend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,22 @@
-function App() {
+import React from 'react';
+import { MapContainer, TileLayer } from 'react-leaflet';
+import 'leaflet/dist/leaflet.css';
 
-  return (
-    <>
-      <h1>First Commit</h1>
-      <h4>Here we go...</h4>
-    </>
-  )
+function Map() {
+    return (
+        <div style={{ height: "100vh", width: "100%" }}>
+            <MapContainer
+                center={[48.46432837962857, 35.04685470263019]}
+                zoom={12}
+                style={{ height: "100%", width: "100%" }}
+            >
+                <TileLayer
+                    url="https://tiles.stadiamaps.com/tiles/alidade_satellite/{z}/{x}/{y}{r}.png"
+                    attribution='&copy; CNES, Distribution Airbus DS, © Airbus DS, © PlanetObserver (Contains Copernicus Data) | &copy; <a href="https://www.stadiamaps.com/" target="_blank">Stadia Maps</a> &copy; <a href="https://openmaptiles.org/" target="_blank">OpenMapTiles</a> &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+                />
+            </MapContainer>
+        </div>
+    );
 }
 
-export default App
+export default Map;
