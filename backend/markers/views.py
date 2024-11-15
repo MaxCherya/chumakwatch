f1f8150805e7f731ef94b71d3a666da0d8af26a0
MaxCherya <maxchergik@gmail.com>
2024-11-15 21:57:35 +0200

    reCAPTCHA backend

--- backend/markers/views.py
+++ backend/markers/views.py
@@ -1,12 +1,35 @@
 from rest_framework import status
 from rest_framework.decorators import api_view
+from django.middleware.csrf import CsrfViewMiddleware
 from rest_framework.response import Response
+from django.http import JsonResponse
 from .models import Marker
 from .serializers import MarkerSerializer
-import logging
+from dotenv import load_dotenv
+import requests
+import os
+
+load_dotenv()
+captcha_secret = os.getenv("RECAPTCHA_SECRET_KEY")
+
+def validate_captcha(captcha_token):
+    url = "https://www.google.com/recaptcha/api/siteverify"
+    payload = {"secret": captcha_secret, "response": captcha_token}
+    response = requests.post(url, data=payload)
+    result = response.json()
+    return result.get("success", False)
 
 @api_view(['POST'])
 def add_marker(request):
+    try:
+        CsrfViewMiddleware().process_view(request, None, (), {})
+    except:
+        return JsonResponse({'error': 'Invalid CSRF token'}, status=403)
+    captcha_token = request.data.get('captcha')
+    if not captcha_token:
+        return JsonResponse({'error': 'CAPTCHA token is missing'}, status=400)
+    if not validate_captcha(captcha_token):
+        return JsonResponse({'error': 'Invalid CAPTCHA'}, status=403)
     serializer = MarkerSerializer(data=request.data)
     if serializer.is_valid():
         serializer.save()
